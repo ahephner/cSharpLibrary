a02d40e4699d3d6bcabf543b3bd5c1b412615f96
AJ Hephner <ajhephner178@gmail.com>
2017-11-15 15:39:35 -0500

    added else if

--- 0.04_Conditional_if_else_if_statement/Program.cs
+++ 0.04_Conditional_if_else_if_statement/Program.cs
@@ -10,6 +10,22 @@ namespace _0._04_Conditional_if_else_if_statement
     {
         static void Main(string[] args)
         {
+            int cutler = 6;
+            int mitch = 10;
+            int rex = 8;
+
+            if (cutler > rex)
+                Console.WriteLine("Rex Grossman threw a lot of pics and fumbled so we got Cutler");
+            else
+                if (1 > cutler)
+                Console.Write("Cutler became the all time leading qb in all stats including interceptions so we drafted Mitch");
+            else
+                if (mitch > 4)
+                Console.WriteLine("Mitch is a rookie qb");
+            else
+                Console.WriteLine("its hopeless");
+
+            Console.ReadLine();
         }
     }
 }
